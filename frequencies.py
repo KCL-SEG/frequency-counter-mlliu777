014919cd7b5d931f02a13c9f9565e44ab009540e
Manlin Liu <manlin.liu@kcl.ac.uk>
2022-09-28 20:28:37 +0100

    Complete tast of counting frequencies

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,10 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        item = str(item)
+        if item in frequencies.keys():
+            frequencies.update({item: frequencies.get(item) + 1})
+        else:
+            frequencies[item] = 1
     return frequencies
